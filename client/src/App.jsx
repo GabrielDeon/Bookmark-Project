33044f0367a673463f6721e12213f8a6a3348700
Glauber Ribeiro <glauberbarbacoviribeiro@gmail.com>
2024-11-12 15:11:32 -0300

    feat: add BookCategory and BookReview

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
 import './App.css';
 import BookForm from './BookForm';
+import BookCategory from './BookCategory';
+import BookReview from './BookReview';
 
 function App() {
   return (
     <div style={{color: '#867070'}}>
       <h1>Book Form</h1>
       <BookForm />
+      <hr />
+      <h1>Book Category</h1>
+      <BookCategory/>
+      <hr />
+      <h1>Book Review</h1>
+      <BookReview/>
     </div>
   );
 }
