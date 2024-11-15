ad0f52696f2026a530ef2323c656b329e0c058ac
Gabriel_Deon <gabriel1deon@gmail.com>
2024-11-15 18:44:01 -0300

    fix(User-Book): This commit fix the post method of User-Book module

--- server/src/user-book/user-book.module.ts
+++ server/src/user-book/user-book.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserBookController } from './user-book.controller';
 import { UserBookService } from './user-book.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [UserBookController],
-  providers: [UserBookService]
+  providers: [UserBookService],
+  imports: [PrismaModule],
 })
 export class UserBookModule {}
